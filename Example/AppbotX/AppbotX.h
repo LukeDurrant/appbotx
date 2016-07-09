8ed4b3449ca58763160f2c575d6db3a96099b5df
Luke Durrant <contact@lukedurrant.com>
2016-07-09 21:34:54 +1000

    Project Setup for Carthage

---
+++ Example/AppbotX/AppbotX.h
@@ -0,0 +1,19 @@
+//
+//  AppbotX.h
+//  AppbotX
+//
+//  Created by Luke Durrant on 9/07/2016.
+//  Copyright © 2016 Appbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppbotX.
+FOUNDATION_EXPORT double AppbotXVersionNumber;
+
+//! Project version string for AppbotX.
+FOUNDATION_EXPORT const unsigned char AppbotXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppbotX/PublicHeader.h>
+
+#import "ABX.h"
